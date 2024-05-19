8d2e5d9db013b99b7a32298ea28c93fd4b9eacc2
Konstantin Bobkov <mrglorf@gmail.com>
2024-05-19 21:32:49 +0400

    add demo domain for socket

--- backend/src/socket/socketManager.ts
+++ backend/src/socket/socketManager.ts
@@ -16,7 +16,8 @@ export default function initializeSocket(server: http.Server) {
         'http://localhost:3001',
         'https://admin.socket.io',
         'http://192.168.43.59:3001',
-        'https://coffee-shop-oms-17dd.onrender.com'
+        'https://coffee-shop-oms-17dd.onrender.com',
+        'https://demo-coffeshop.onrender.com'
       ],
       methods: ['GET', 'POST'],
       credentials: true,
