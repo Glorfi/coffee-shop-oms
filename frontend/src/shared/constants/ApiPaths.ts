60582336e28d5f4f6e4e6ec701cf92fafb4aeb75
Konstantin Bobkov <mrglorf@gmail.com>
2024-05-19 06:14:43 +0400

    demo deploy config

--- frontend/src/shared/constants/ApiPaths.ts
+++ frontend/src/shared/constants/ApiPaths.ts
@@ -4,10 +4,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 export const API_PATH = {
   //  BASE: 'http://localhost:4000/api',
   // 'https://coffee-shop-oms.onrender.com/api',
-  BASE:
-    hostname === 'localhost'
-      ? 'http://localhost:4000/api'
-      : process.env.BACKEND_LINK,
+  BASE: !isProduction ? 'http://localhost:4000/api' : process.env.VITE_BACKEND_LINK,
   CATEGORIES: '/categories',
   DRINKS: '/drinks',
   ORDERS: '/orders',
